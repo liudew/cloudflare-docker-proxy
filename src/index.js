226dc1f713819c650c4b34efa126b6d5cde986cb
liudew <33311272+liudew@users.noreply.github.com>
2024-07-16 11:14:01 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.runbao.link": dockerHub,
+  "quay.runbao.link": "https://quay.io",
+  "gcr.runbao.link": "https://gcr.io",
+  "k8s-gcr.runbao.link": "https://k8s.gcr.io",
+  "k8s.runbao.link": "https://registry.k8s.io",
+  "ghcr.runbao.link": "https://ghcr.io",
+  "cloudsmith.runbao.link": "https://docker.cloudsmith.io",
+  "ecr.runbao.link": "https://public.ecr.aws",
 
   // staging
   "docker-staging.libcuda.so": dockerHub,
